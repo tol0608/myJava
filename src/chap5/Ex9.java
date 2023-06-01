d9591870c693e5880f11724573432a3d6799b1c2
jaehyeonlee <jerry0608s@gmail.com>
2023-06-01 15:17:15 +0900

    ex9 setting

---
+++ src/chap5/Ex9.java
@@ -0,0 +1,12 @@
+package chap5;
+
+import java.util.Scanner;
+
+//사용자에게 3명의 학생에 대한 국어, 영어, 수학, 과학 점수를 입력 받아 1등인 학생의 성적과 총점, 평균을 출력하는 코드를 작성
+public class Ex9 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+	}
+}
